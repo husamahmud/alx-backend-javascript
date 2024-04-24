3369493d87af2f28b903f6d84d78e1edde07932d
husamahmud <husamahmud@gmail.com>
2024-04-25 01:29:48 +0300

    Add 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) resolve({ status: 200, body: 'Success' });
-    reject(new Error(''));
-  }).finally(() => console.log('Got a response from the API'));
+  return promise.then(() => {
+    console.log('Got a response from the API');
+    return { status: 200, body: 'Success' };
+  }).catch(() => {
+    console.log('Got a response from the API');
+    return new Error();
+  });
 }
