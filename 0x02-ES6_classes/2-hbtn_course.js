ff6a2a690c78a56da5000f10ef87cda0b3af876b
husamahmud <husamahmud@gmail.com>
2024-04-30 01:09:20 +0300

    Add 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,16 +1,16 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== "string") {
-      throw TypeError("Name must be a string");
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
     }
-    if (typeof length !== "number") {
-      throw TypeError("Length must be a number");
+    if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
     }
     if (!Array.isArray(students)) {
-      throw TypeError("Students must be an array");
+      throw TypeError('Students must be an array');
     }
-    if (students.some((student) => typeof student !== "string")) {
-      throw TypeError("Students must be an array of strings");
+    if (students.some((student) => typeof student !== 'string')) {
+      throw TypeError('Students must be an array of strings');
     }
     this._name = name;
     this._length = length;
