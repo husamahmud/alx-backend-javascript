c2c362d688a31ff6c4e41c053306bec52ed9ba12
husamahmud <husamahmud@gmail.com>
2024-04-30 00:42:47 +0300

    Add 1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,8 +2,8 @@ import ClassRoom from "./0-classroom.js";
 
 function initializeRooms() {
   let res = [];
-  rooms.push(new ClassRoom(19));
-  rooms.push(new ClassRoom(20));
-  rooms.push(new ClassRoom(34));
+  res.push(new ClassRoom(19));
+  res.push(new ClassRoom(20));
+  res.push(new ClassRoom(34));
   return res;
 }
