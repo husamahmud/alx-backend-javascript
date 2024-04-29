7218fd0fd8ce63ff9c2d0ffb62f11d01f0d7b3fe
husamahmud <husamahmud@gmail.com>
2024-04-30 01:23:36 +0300

    Add 3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,21 +1,26 @@
 export default class Currency {
   constructor(code, name) {
-    if (typeof name === 'string') this._name = name;
-    else throw TypeError('Name must be a string');
-
-    if (typeof code === 'string') this._code = code;
-    else throw TypeError('Name must be a string');
-  }
-
-  get name() {
-    return this._name;
+    this._code = code;
+    this._name = name;
   }
 
   get code() {
     return this._code;
   }
 
-  get displayFullCurrency() {
-    return `${this.name} (${this.code})`;
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
   }
 }
