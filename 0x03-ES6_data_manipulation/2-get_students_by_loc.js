52da258c8234c55053e58fbc6e68f7f66ba41c16
husamahmud <husamahmud@gmail.com>
2024-05-01 02:51:30 +0300

    Add 2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
-export default function getStudentsByLocation(students) {
-  return students.filter((student) => student.location === 'San Francisco');
+export default function getStudentsByLocation(students, location) {
+  return students.filter((student) => student.location === location);
 }
