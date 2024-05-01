ee02d33947a561448141af96753446e59da67b43
husamahmud <husamahmud@gmail.com>
2024-05-01 03:41:18 +0300

    Add 9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const groceryMap = new Map();
+  groceryMap.set('apple', 5);
+  groceryMap.set('banana', 3);
+  groceryMap.set('bread', 2);
+  groceryMap.set('milk', 1);
+  groceryMap.set('eggs', 12);
+  return groceryMap;
+}
